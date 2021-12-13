eb3b94bb25624fc8e429567467abefbb802f41d5
Davide Buccella <buccella.davide@gmail.com>
2021-12-14 00:00:48 +0100

    fix description

--- MySortingTable-min.js
+++ MySortingTable-min.js
@@ -1,6 +1,6 @@
 /**
  * @fileOverview 
- *  Simple class to perform the table sort: was created specifically to compensate for the fact that - sometimes - sorting datatables cannot be applied. 
+ *  A light simple class to perform the table sort: was created specifically to compensate for the fact that - sometimes - sorting datatables cannot be applied. 
  *  The other usefulness is that it's a single file. It does not depends on any other file (css, javascript, etc.)
  *  Add the .toSort class to the th to be sorted.
  *  Add the .defaultSort class to the th to be sorted immediately upon loading the table.
